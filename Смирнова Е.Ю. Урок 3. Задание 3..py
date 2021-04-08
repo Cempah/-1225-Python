b6d11c064f5fe22a5fb4444e1dda71c055440ffa
KatSmi <cempah@mail.ru>
2021-04-08 19:06:30 +0300

    ss

--- Смирнова Е.Ю. Урок 3. Задание 3..py
+++
@@ -1,9 +0,0 @@
-def my_func():
-    a1 = int(input("Введите число 1: "))
-    a2 = int(input("Введите число 2: "))
-    a3 = int(input("Введите число 3: "))
-    a4 = min(a1, a2, a3)
-    return (a1 + a2 + a3) - a4
-
-
-print(my_func())
--- Смирнова Е.Ю. Урок 3. Задание 6..py
+++
@@ -1,8 +0,0 @@
-def int_func(s):
-    for i in range(len(s)):
-        s[i] = s[i].title()
-    return " ".join(s)
-
-
-s1 = input("Введите слова из маленьких латинских букв: ").split()
-print(int_func(s1))
